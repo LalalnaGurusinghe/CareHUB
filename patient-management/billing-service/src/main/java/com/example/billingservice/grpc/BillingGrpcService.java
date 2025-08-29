017a78e8be3869318b5a9e52f477f7417336b68c
Lalana Malintha Gurusinghe <153848846+LalalnaGurusinghe@users.noreply.github.com>
2025-08-29 14:24:03 +0530

    Extend BillingGrpcService with gRPC implementation
    BillingGrpcService now extends BillingServiceGrpc.BillingServiceImplBase and includes a stub for the createBillingAccount method, preparing the service for gRPC request handling.

--- patient-management/billing-service/src/main/java/com/example/billingservice/grpc/BillingGrpcService.java
+++ patient-management/billing-service/src/main/java/com/example/billingservice/grpc/BillingGrpcService.java
@@ -1,7 +1,15 @@
 package com.example.billingservice.grpc;
 
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
-public class BillingGrpcService {
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<billing.BillingResponse> responseObserver) {
+        
+    }
 }
